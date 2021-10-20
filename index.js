d7f9d865379820dea2ad1d4f10024e267a284bde
Jota <jjimenez@zonar.cl>
2021-10-20 13:45:47 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const log = require("./lib/Logs")
 const downloader = require("./lib/Downloader")
 
+console.log("Starting copernicus downloader ...");
 downloader.init();
 log.info("Coernicus downloader initialized");
